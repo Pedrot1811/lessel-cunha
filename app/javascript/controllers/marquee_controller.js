b2ed8c32023254e2c8f3a590870dca8a5e538331
Pedro Toledo <pedrot1811@gmail.com>
2025-04-30 14:47:11 -0300

    autoscroll marquee working

--- app/javascript/controllers/marquee_controller.js
+++ app/javascript/controllers/marquee_controller.js
@@ -1,12 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 import EmblaCarousel from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay'
+import Autoscroll from 'embla-carousel-auto-scroll'
 
 // Connects to data-controller="marquee"
 export default class extends Controller {
   connect() {
     const emblaNode = this.element
-    const options = { loop: false }
-    const emblaApi = EmblaCarousel(emblaNode, options)
+    const emblaApi = EmblaCarousel(emblaNode, { loop: true }, [
+      Autoscroll({ speed: 2 }, { startDelay: 20 }, { stopOnInteraction: true }, { stopOnMouseEnter: true }, { stopOnFocusIn: false })
+    ])
     console.log(emblaApi.slideNodes())
   }
 }
